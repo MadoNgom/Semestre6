b94fe17cdb2209d2637a1ed2e678f8dbf8de2a6d
Mado Ngome <maladongom104@gmail.com>
2024-08-04 20:36:56 +0000

    reviews code

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ require('DBTransaction.php');
 $transaction = new DBTransaction();
 $produits = $transaction->getAllProduct();
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,9 +18,7 @@ $produits = $transaction->getAllProduct();
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <!-- BOOSTRAP CDN LINK -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
     <link rel="stylesheet" href="./style.css">
-
 </head>
 
 <body>
@@ -273,8 +270,9 @@ $produits = $transaction->getAllProduct();
     <!-- Nos partenaires -->
     <section>
         <div class="container">
-            Nos partenaires
-            .
+            <h5>
+                Nos partenaires
+            </h5>
         </div>
     </section>
     <footer class="container-fluid text-white text-center text-lg-start bg-dark">
